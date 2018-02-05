dd15aa7e61fc571c726fe0eb0018339d3e568c54
JiahuiRuan <jr58@rice.edu>
2018-02-05 14:31:39 -0800

    start to use pthread

---
+++ PThread/cmake-build-debug/CMakeFiles/PThread.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PThread.dir/main.c.o"
+  "PThread.pdb"
+  "PThread"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PThread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
